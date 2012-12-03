046c46471765273dac9a29e4130ad1a4590760e2
Dominik D. Geyer <dominik.geyer@gmail.com>
2012-12-04 00:14:29 +0100

    Implemented simple message function (echo to stderr).

--- confcan.sh
+++ confcan.sh
@@ -7,47 +7,50 @@ TIMEOUT=5
 
 ###############################
 
-if [[ $# != 1 ]]; then
-    echo "Usage..." >&2
-    exit
-fi
-
-REPO_DIR=$(readlink -f "$1") || { echo "Target does not exist." >&2; exit 1; }
-[[ ! -d "$REPO_DIR" ]] && { echo "Error: The target is not a directory." >&2; exit 1; }
-cd "$REPO_DIR"
-
-###############################
+msg () {
+	echo "$@" >&2
+}
 
 git_trigger () {
 	if ! git add .; then
-		echo "WARN: git add failed" >&2
+		msg "WARN: git add failed"
 		return 1
 	fi
 	
 	if [[ -n "$(git status --porcelain)" ]]; then
 		if ! git commit -a -m "Auto commit $(date +'%Y-%m-%d %H:%M:%S')"; then
-			echo "WARN: git commit failed" >&2
+			msg "WARN: git commit failed"
 			return 2
 		fi
 	else
-		echo "Nothing to do... skipping commit!" >&2
+		msg "Nothing to do... skipping commit!"
 	fi
 }
 
 cleanup () {
-	echo -e "\n$FUNCNAME" >&2
 	if [[ -n "$SLEEP_PID" ]] && kill -0 $SLEEP_PID &>/dev/null; then
-		echo "CLEANUP: killing $SLEEP_PID" >&2
+		msg "CLEANUP: killing $SLEEP_PID"
 		kill $SLEEP_PID &>/dev/null
 	fi
 	exit 0
 }
 
 usr_timeout () {
-	git_trigger || echo "... something went wrong with git_trigger()" >&2
+	git_trigger || msg "... something went wrong with git_trigger()"
 	
 	return 0
 }
+
+###############################
+
+if [[ $# != 1 ]]; then
+    echo "Usage..." >&2
+    exit
+fi
+
+REPO_DIR=$(readlink -f "$1") || { msg "Error: Repository does not exist."; exit 1; }
+[[ ! -d "$REPO_DIR" ]] && { msg "Error: The repository is not a directory."; exit 1; }
+cd "$REPO_DIR" || { msg "Error: Cannot change into repository directory."; exit 1; }
 
 ###############################
 
@@ -62,19 +65,15 @@ while read -r line; do
 	echo "NOTIFY $(printf '%05d' $a): ${line/$REPO_DIR/GIT_REPO}"
 	
 	if [[ -n "$SLEEP_PID" ]] && kill -0 $SLEEP_PID &>/dev/null; then
-		#echo "DEFERRED timeout; killing $SLEEP_PID" >&2
 		kill $SLEEP_PID &>/dev/null || true
 		wait $SLEEP_PID &>/dev/null || true
 	fi
 	
 	# timeout subshell
 	(
-		#echo "waiting some time..." >&2
 		sleep $TIMEOUT
-		#echo "timeout... sending signal USR1 to parent $PID" >&2
-		kill -SIGUSR1 $PID || echo "WARN: Error sending USR1 to $PID" >&2
+		kill -SIGUSR1 $PID || msg "WARN: Error sending USR1 to $PID"
 	) &
 	SLEEP_PID=$!
 done < <(inotifywait -m -r -e $INW_EVENTS "$REPO_DIR" "@${REPO_DIR}/.git" 2>/dev/null)
-
 
