3dbcad52953cdef70c92ac68154f60986d4314f4
Dominik D. Geyer <dominik.geyer@gmail.com>
2012-12-08 03:08:48 +0100

    Use new function cerrexit() for fatal errors.

--- confcan.sh
+++ confcan.sh
@@ -35,10 +35,18 @@ usage () {
 	EOT
 }
 
+# Print message out to stderr
 cmsg () {
 	echo "$@" >&2
 }
 
+# Print error message out to stderr and exit
+cerrexit () {
+	echo "$@" >&2
+	exit 1
+}
+
+# Print informational message out to stderr (depending on verbosity level)
 cinfo () {
 	(( VERBOSITY < 1 )) && return
 	echo "$@" >&2
@@ -47,7 +55,7 @@ cinfo () {
 git_trigger () {
 	# stage all changed/new/deleted files
 	if ! $GIT add --all -- "${GIT_ADD_DIRS[@]}"; then
-		cmsg "Warning: git add failed"
+		cmsg "Warning: git add failed."
 		return 1
 	fi
 	
@@ -55,7 +63,7 @@ git_trigger () {
 	if [[ -n "$($GIT status --porcelain -- "${GIT_ADD_DIRS[@]}")" ]]; then
 		# commit staged changes
 		if ! $GIT commit --message "Auto commit $(date +'%Y-%m-%d %H:%M:%S')"; then
-			cmsg "Warning: git commit failed"
+			cmsg "Warning: git commit failed."
 			return 2
 		fi
 	else
@@ -71,7 +79,7 @@ cleanup () {
 
 # Signal handler for USR1 which triggers an action
 usr_timeout () {
-	git_trigger || cmsg "Warning: Smething went wrong with git_trigger()"
+	git_trigger || cmsg "Warning: Not all git actions were successful."
 	
 	return 0
 }
@@ -108,6 +116,9 @@ declare -a GIT_ADD_DIRS
 declare INW_EVENTS="create,close_write,moved_to,move_self,delete"
 declare -i VERBOSITY=0
 
+# invoked without any arguments -> print usage to stdout and exit with success
+[[ $# == 0 ]] && { usage; exit 0; }
+
 while getopts ":t:a:ice:vh" opt; do
 	case $opt in
 	t) # timeout in seconds for timeout task
@@ -129,25 +140,25 @@ while getopts ":t:a:ice:vh" opt; do
 		VERBOSITY=$((VERBOSITY + 1))
 		;;
 	h)
-		usage >&2
+		usage
 		exit 0
 		;;
 	\?)
-		cmsg "Error: Invalid option -$OPTARG"
+		cmsg "Error: Invalid option -$OPTARG."
 		usage >&2
 		exit 1
 		;;
 	:)
-		cmsg "Error: Option -$OPTARG requires an argument"
+		cmsg "Error: Option -$OPTARG requires an argument."
 		usage >&2
 		exit 1
 		;;
 	esac
 done
 
-shift $((OPTIND-1))		# shift options
-unset opt OPTIND		# unset variables used for option parsing
+shift $((OPTIND-1))
 
+# is required argument for repository missing?
 if [[ $# != 1 ]]; then
     usage >&2
     exit 1
@@ -155,30 +166,29 @@ fi
 
 
 # determine canonical path of repository, as inotifywait seems not to deal with symlinks
-REPO_DIR=$(readlink -f "$1") || { cmsg "Error: Base directory does not exist."; exit 1; }
+REPO_DIR=$(readlink -f "$1") || cerrexit "Error: Base directory does not exist."
 
 if [[ -z "${GIT_INIT-}" ]]; then
 	# sanity check: is this a git repository?
-	[[ -d "$REPO_DIR/.git" ]] || { cmsg "Error: Directory '$REPO_DIR' is not a Git repository."; exit 1; }
+	[[ -d "$REPO_DIR/.git" ]] || cerrexit "Error: Directory '$REPO_DIR' is not a Git repository."
 else
 	# initialize Git repository if requested by option '-i'
 	cinfo "Initializing Git repository '$REPO_DIR'."
-	$GIT init "$REPO_DIR" &>/dev/null || { cmsg "Error: Could not initialize Git repository '$REPO_DIR'."; exit 1; }
+	$GIT init "$REPO_DIR" &>/dev/null || cerrexit "Error: Could not initialize Git repository '$REPO_DIR'."
 	
 	if (( ${#GIT_ADD_DIRS[@]} > 0 )); then
 		for d in "${GIT_ADD_DIRS[@]}"; do
 			d="$REPO_DIR/$d"
 			if [[ ! -d "$d" ]]; then
 				cinfo "Creating directory '$d'."
-				mkdir -p "$d" || { cmsg "Error: Could not create directory '$d'."; exit 1; }
+				mkdir -p "$d" || cerrexit "Error: Could not create directory '$d'."
 			fi
 		done
-		unset d
 	fi
 fi
 
 # chdir into repository, as git used to operate within its repository
-cd "$REPO_DIR" || { cmsg "Error: Cannot change into repository directory."; exit 1; }
+cd "$REPO_DIR" || cerrexit "Error: Cannot change into repository directory."
 
 
 # selective directory watches
@@ -189,10 +199,9 @@ fi
 declare -a INW_DIRS
 for d in "${GIT_ADD_DIRS[@]}"; do
 	d="$REPO_DIR/$d"
-	[[ -d "$d" ]] || { cmsg "Error: Directory '$d' does not exist."; exit 1; }
+	[[ -d "$d" ]] || cerrexit "Error: Directory '$d' does not exist."
 	INW_DIRS+=( "$(readlink -f "$d")" )
 done
-unset d
 
 # stage and commit all changes before monitoring?
 if [[ -n "${GIT_INITCOMMIT-}" ]]; then
